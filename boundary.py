caa054d3cf3c6b7dbfa90ed1ca8fb8b327c3a98c
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2021-02-10 00:29:51 +0600

    Delete boundary.py

--- boundary.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-class BC:
-    def fixed(nelx,nely):
-        # BC's and support
-        dofs=np.arange(2*(nelx+1)*(nely+1))
-        #fixed=np.union1d(np.array([80]),dofs[8050:8060:1])
-        #fixed=np.union1d(dofs[0:2*(nely+1):2],np.array([2*(nelx+1)*(nely+1)-1]))
-        fixed=np.union1d(dofs[0:2*(nely+1):1],[0])
-        return fixed
